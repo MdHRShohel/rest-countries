9cfc0b1940ec7bbd9524e8a321f1bde10cbb31c3
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-09-18 23:21:01 +0600

    flag added

--- js/countries.js
+++ js/countries.js
@@ -1,9 +1,26 @@
 // api link: https://restcountries.com/v2/all
 
 const loadCountries = () => {
-    fetch('https://restcountries.com/v2/all')
+    fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => displayCountries(data))
+}
+
+const displayCountries = countries => {
+    //console.log(countries);
+    const CountriesHTML = countries.map(country => getCountryHTML(country));
+    //console.log(CountriesHTML[11]);
+    const container = document.getElementById('countries');
+    container.innerHTML = CountriesHTML.join(' ');
+};
+
+const getCountryHTML = country => {
+    return `
+        <div>
+            <h2>${country.name?.common}</h2>
+            <img src="${country.flags.png}">
+        <div>
+    `
 }
 
 loadCountries();
